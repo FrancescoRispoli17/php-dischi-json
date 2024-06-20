bf38ff30942a1928920b10731e018b291109b0a8
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-06-20 21:10:35 +0200

    [UPD] call server.php to memorize data in main.js

--- main.js
+++ main.js
@@ -3,11 +3,18 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-           
+            server_url:'http://localhost/php-dischi-json/server.php',
+            discs:[],
         }
     },
     methods:{
-        
+        getDisc(){
+            axios.get(this.server_url).then((response)=>{
+                this.discs = response.data;
+            })
+        }
     },
-
+    created(){
+        this.getDisc();
+    }
 }).mount('#app');
