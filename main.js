1d23c731d68b703409699675ae9598b00a44ae9e
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-06-21 23:03:04 +0200

    [UPD] add disc

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ createApp({
             discs:[],
             currentFocus:'',
             focusDisc:[],
+            newName:'',
+            newAuthor:'',
+            newYear:'',
+            newUrl:'',
+            newDescription:'',
         }
     },
     methods:{
@@ -25,6 +30,23 @@ createApp({
                 this.currentFocus=true;
                 this.focusDisc = response.data;
             })
+        },
+        addDisc(){
+            const data={
+                action:'create',
+                name:this.newName,
+                author:this.newAuthor,
+                year:this.newYear,
+                url:this.newUrl,
+                description:this.newDescription,
+            }
+            axios.post(this.server_url, data,{
+                headers:{'Content-type':'multipart/form-data'},
+            }).then(response=>{
+                this.discs=response.data;
+                console.log(response.data)
+            })
+            console.log(data)
         }
     },
     created(){
--- server/server.php
+++ server/server.php
@@ -13,6 +13,19 @@ require_once __DIR__ . '/function.php';
         if($position!==false)
             $dischi_light=$dischi[$position];
     }
+    if(isset($_POST['action']) && $_POST['action']==='create'){
+        $new_disc=[
+            "id"=> count($dischi),
+            "name"=>$_POST['name'],
+            "author"=>$_POST['author'],
+            "year"=>$_POST['year'],
+            "url"=>$_POST['url'],
+            "description"=>$_POST['description']
+        ];
+        $dischi_light=[...$dischi, $new_disc];
+        $dischi_light=array_map('getDisc',$dischi_light);
+        file_put_contents(__DIR__ . '/disc.json', json_encode($dischi_light));
+    }
     header('Content-Type: application/json');
     echo json_encode($dischi_light);
 
